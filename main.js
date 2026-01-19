fbc753d30be9f3005629ac2bf879fe5534b16fca
fleshkani <nitrorabbitflex@gmail.com>
2026-01-19 22:56:07 +0500

    refactor names

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 //все карточки сделать одним цветом
 const productCards = document.querySelectorAll('.card');
-const changeColorCardsButton = document.querySelector('#change-color-all-cards');
+const buttonCardsColorСhange = document.querySelector('#all-cards-color-change');
 const greenColorHash = '#00FF00';
-changeColorCardsButton.addEventListener('click', () => {
+buttonCardsColorСhange.addEventListener('click', () => {
   productCards.forEach((card) => card.style.backgroundColor = greenColorHash)
 });
 
 //поменять фон первой карточки
-const firstproductCard = document.querySelector('.card');
-const changeColorCardButton = document.querySelector('#change-color-first-card');
+const firstProductCard = document.querySelector('.card');
+const buttonCardColorСhange = document.querySelector('#card-color-first-change');
 const redColorHash = '#ff0000ff';
-changeColorCardButton.addEventListener('click', () => {
-  firstproductCard.style.backgroundColor = redColorHash;
+buttonCardColorСhange.addEventListener('click', () => {
+  firstProductCard.style.backgroundColor = redColorHash;
 });
 
 //открыть страницу гугл
-const openGoogleButton = document.querySelector('#open-google');
-openGoogleButton.addEventListener('click', openGoogle);
+const buttonGoogleOpen = document.querySelector('#google-open');
+buttonGoogleOpen.addEventListener('click', openGoogle);
 
 //вопрос подтверждение
 function openGoogle() {
@@ -29,25 +29,25 @@ function openGoogle() {
 };
 
 // вывод алерт и консоль лог
-const outputConsoleLogButton = document.querySelector('#output-console-log');
-outputConsoleLogButton.addEventListener('click', () => outputConsoleLog('ДЗ№4'));
+const buttonConsoleLogOutput = document.querySelector('#console-log-output');
+buttonConsoleLogOutput.addEventListener('click', () => outputConsoleLog('ДЗ№4'));
 
 function outputConsoleLog(message) {
   alert('alert!')
   console.log(message)
 };
 
-// вывод эвент
-const chooseYourProduct = document.querySelector('.title');
-chooseYourProduct.addEventListener('mouseenter', (event) => {
+// при наведении на заголовок 'Выбери свой продукт' элемент выводится в консоль
+const yourProductChoose = document.querySelector('.title');
+yourProductChoose.addEventListener('mouseenter', (event) => {
   console.log(event.target);
 });
 
 //сменить цвет кнопки
-const changeColorButton = document.querySelector('#change-color-button');
-changeColorButton.classList.add('red');
-changeColorButton.addEventListener('click', () => {
-  changeColorButton.classList.toggle('red');
-  changeColorButton.classList.toggle('green');
+const buttonColorСhange = document.querySelector('#button-color-change');
+buttonColorСhange.classList.add('red');
+buttonColorСhange.addEventListener('click', () => {
+  buttonColorСhange.classList.toggle('red');
+  buttonColorСhange.classList.toggle('green');
 });
 
